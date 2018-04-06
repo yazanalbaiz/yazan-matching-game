d171f0125910bb889d9d69a99bf19f3a60b6f48a
Yazan AL-Baiz <yazobeats@gmail.com>
2018-04-06 21:27:38 +0300

    added: shuffle function
    title

--- js/app.js
+++ js/app.js
@@ -18,7 +18,20 @@ const iconsArray = [
     '<i class="fas fa-leaf"></i>'
 ];
 
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
 
 
 document.addEventListener('click', (e) => {
