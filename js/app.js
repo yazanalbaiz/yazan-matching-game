b7f01e3c6401dcd19746bc3e781b0ced23034f37
Yazan AL-Baiz <yazobeats@gmail.com>
2018-04-06 21:26:28 +0300

    added: icons array
    added icons array in app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
+const iconsArray = [
+    '<i class="fas fa-rocket"></i>',
+    '<i class="fas fa-bell"></i>',
+    '<i class="fas fa-bug"></i>',
+    '<i class="fas fa-chess-knight"></i>',
+    '<i class="far fa-smile"></i>',
+    '<i class="fas fa-dove"></i>',
+    '<i class="fas fa-eye"></i>',
+    '<i class="fas fa-leaf"></i>',
+    // Repeat
+    '<i class="fas fa-rocket"></i>',
+    '<i class="fas fa-bell"></i>',
+    '<i class="fas fa-bug"></i>',
+    '<i class="fas fa-chess-knight"></i>',
+    '<i class="far fa-smile"></i>',
+    '<i class="fas fa-dove"></i>',
+    '<i class="fas fa-eye"></i>',
+    '<i class="fas fa-leaf"></i>'
+];
+
+
+
+
 document.addEventListener('click', (e) => {
     if(e.target.getAttribute('class') === 'game-card-hidden')
         e.target.setAttribute('class', 'game-card-revealed');
