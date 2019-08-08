c9d4cba9ac91c0eb585de96ed8666acfa085405e
y-albaiz <y.albaiz@zid.sa>
2019-08-08 21:16:11 +0300

    added vibration if incorrect move

--- js/app.js
+++ js/app.js
@@ -23,215 +23,251 @@ let lastTime;
 
 //Array of cards
 let iconsArray = [
-    '<i class="fab fa-firefox"></i>',
-    '<i class="fab fa-react"></i>',
-    '<i class="fab fa-linux"></i>',
-    '<i class="fab fa-empire"></i>',
-    '<i class="fab fa-github"></i>',
-    '<i class="fab fa-pied-piper-alt"></i>',
-    '<i class="fab fa-rebel"></i>',
-    '<i class="fab fa-reddit-alien"></i>',
-    // Repeat
-    '<i class="fab fa-firefox"></i>',
-    '<i class="fab fa-react"></i>',
-    '<i class="fab fa-linux"></i>',
-    '<i class="fab fa-empire"></i>',
-    '<i class="fab fa-github"></i>',
-    '<i class="fab fa-pied-piper-alt"></i>',
-    '<i class="fab fa-rebel"></i>',
-    '<i class="fab fa-reddit-alien"></i>'
+  '<i class="fab fa-firefox"></i>',
+  '<i class="fab fa-react"></i>',
+  '<i class="fab fa-linux"></i>',
+  '<i class="fab fa-empire"></i>',
+  '<i class="fab fa-github"></i>',
+  '<i class="fab fa-pied-piper-alt"></i>',
+  '<i class="fab fa-rebel"></i>',
+  '<i class="fab fa-reddit-alien"></i>',
+  // Repeat
+  '<i class="fab fa-firefox"></i>',
+  '<i class="fab fa-react"></i>',
+  '<i class="fab fa-linux"></i>',
+  '<i class="fab fa-empire"></i>',
+  '<i class="fab fa-github"></i>',
+  '<i class="fab fa-pied-piper-alt"></i>',
+  '<i class="fab fa-rebel"></i>',
+  '<i class="fab fa-reddit-alien"></i>'
 ];
-
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 /* End of shuffle function */
 
 //Penalty for extra moves
 function moveChecker() {
-    if(moves > 16 && moves <= 28){
-        starsDiv.children[2].setAttribute('data-prefix', 'far');
-        stars = 2;
-    }
-    else if (moves > 28 && moves <= 44 ){
-        starsDiv.children[1].setAttribute('data-prefix', 'far');
-        stars = 1;
-    }
-    else if (moves > 44){
-        starsDiv.children[0].setAttribute('data-prefix', 'far');
-        stars = 0;
-    }
-    else if(moves <= 16){
-        starsDiv.children[0].setAttribute('data-prefix', 'fas');
-        starsDiv.children[1].setAttribute('data-prefix', 'fas');
-        starsDiv.children[2].setAttribute('data-prefix', 'fas');
-        stars = 3;
-    }
+  if (moves > 16 && moves <= 28) {
+    starsDiv.children[2].setAttribute('data-prefix', 'far');
+    stars = 2;
+  } else if (moves > 28 && moves <= 44) {
+    starsDiv.children[1].setAttribute('data-prefix', 'far');
+    stars = 1;
+  } else if (moves > 44) {
+    starsDiv.children[0].setAttribute('data-prefix', 'far');
+    stars = 0;
+  } else if (moves <= 16) {
+    starsDiv.children[0].setAttribute('data-prefix', 'fas');
+    starsDiv.children[1].setAttribute('data-prefix', 'fas');
+    starsDiv.children[2].setAttribute('data-prefix', 'fas');
+    stars = 3;
+  }
 }
 /* End of moveChecker function */
 
 //Reveals a card and checks if it matches a revealed or not
 function revealAndCheck(e) {
-    if(e.target.getAttribute('class').includes('game-card')){
-        showMoves();
-        const target = e.target;
-    
-        //Checks if the card is unflipped and flips it if so
-        if(target.getAttribute('class').includes('hidden'))
-            target.setAttribute('class', target.getAttribute('class').replace(/hidden/gi, 'revealed'));
-    
-        target.firstElementChild.style.display = 'block';
-        checkMatched(target);
-        if(matches === 8){
-            openModal();
-        }
-        checkUnmatched(target);
-        showMoves();
-        moveChecker();
+  if (e.target.getAttribute('class').includes('game-card')) {
+    showMoves();
+    const target = e.target;
+
+    //Checks if the card is unflipped and flips it if so
+    if (target.getAttribute('class').includes('hidden'))
+      target.setAttribute(
+        'class',
+        target.getAttribute('class').replace(/hidden/gi, 'revealed')
+      );
+
+    target.firstElementChild.style.display = 'block';
+    checkMatched(target);
+    if (matches === 8) {
+      openModal();
     }
+    checkUnmatched(target);
+    showMoves();
+    moveChecker();
+  }
 }
 /* End of revealAndCheck funtion */
 
 //Fils the board with random cards
 function addRandomIcons() {
-    matches = 0;
-    moves = 0;
-    moveChecker();
-    shuffle(iconsArray);
-    let newArray = [];
+  matches = 0;
+  moves = 0;
+  moveChecker();
+  shuffle(iconsArray);
+  let newArray = [];
 
-    for(let card of initCards) {
-        card.setAttribute('class', 'game-card revealed');
+  for (let card of initCards) {
+    card.setAttribute('class', 'game-card revealed');
+  }
+
+  for (let card of initCards) {
+    let icon = iconsArray.pop();
+    newArray.push(icon);
+    card.innerHTML = icon;
+    icon = card.firstElementChild;
+    icon.setAttribute('class', icon.getAttribute('class') + ' fa-4x');
+  }
+
+  iconsArray = newArray;
+
+  setTimeout(() => {
+    for (let card of initCards) {
+      let icon = card.firstElementChild;
+      icon.style.display = 'none';
+      let iconClass = icon.getAttribute('class').split(' ');
+      iconClass[3] = 'fa-2x';
+      icon.setAttribute(
+        'class',
+        iconClass[0] +
+          ' ' +
+          iconClass[1] +
+          ' ' +
+          iconClass[2] +
+          ' ' +
+          iconClass[3]
+      );
+      card.setAttribute(
+        'class',
+        'game-card hidden ' + icon.getAttribute('class').substr(7)
+      );
+      hideCards(card);
     }
-
-    for(let card of initCards) {
-        let icon = iconsArray.pop();
-        newArray.push(icon);
-        card.innerHTML = icon;
-        icon = card.firstElementChild;
-        icon.setAttribute('class', icon.getAttribute('class') + ' fa-4x');
-    }
-
-    iconsArray = newArray;
-
-    setTimeout(() => {
-        for(let card of initCards) {
-            let icon = card.firstElementChild;
-            icon.style.display = 'none';
-            let iconClass = icon.getAttribute('class').split(' ');
-            iconClass[3] = 'fa-2x';
-            icon.setAttribute('class', iconClass[0]+' '+iconClass[1]+' '+iconClass[2]+' '+iconClass[3]);
-            card.setAttribute('class', 'game-card hidden ' + icon.getAttribute('class').substr(7));
-            hideCards(card);
-        }
-    }, 1000);
-    
+  }, 1000);
 }
 /* End of addRandomIcons funtion */
 
 //Hides a passed card
 function hideCards(card) {
-        if(card.getAttribute('class').includes('revealed')){
-            card.setAttribute('class', card.getAttribute('class').replace(/revealed/gi, 'hidden'));
-            card.firstElementChild.style.display = 'none';
-        }
-        if(card.getAttribute('class').includes('false')){
-            card.setAttribute('class', card.getAttribute('class').replace(/false/gi, 'hidden'));
-            card.firstElementChild.style.display = 'none';
-        }
+  if (card.getAttribute('class').includes('revealed')) {
+    card.setAttribute(
+      'class',
+      card.getAttribute('class').replace(/revealed/gi, 'hidden')
+    );
+    card.firstElementChild.style.display = 'none';
+  }
+  if (card.getAttribute('class').includes('false')) {
+    card.setAttribute(
+      'class',
+      card.getAttribute('class').replace(/false/gi, 'hidden')
+    );
+    card.firstElementChild.style.display = 'none';
+  }
 }
 /* End of hideCards funtion */
 
 //Checks if the target matches a revealed card then makes them both blue if so
 function checkMatched(target) {
-    for(let card of initCards) {
-        if(card !== target && card.getAttribute('class') === target.getAttribute('class')){
-            card.setAttribute('class', card.getAttribute('class').replace(/revealed/gi, 'true'));
-            target.setAttribute('class', target.getAttribute('class').replace(/revealed/gi, 'true'));
-            matches++;
-            moves++;
-            break;
-        }
+  for (let card of initCards) {
+    if (
+      card !== target &&
+      card.getAttribute('class') === target.getAttribute('class')
+    ) {
+      card.setAttribute(
+        'class',
+        card.getAttribute('class').replace(/revealed/gi, 'true')
+      );
+      target.setAttribute(
+        'class',
+        target.getAttribute('class').replace(/revealed/gi, 'true')
+      );
+      matches++;
+      moves++;
+      break;
     }
+  }
 }
 /* End of checkMatched funtion */
 
 //Checks if the target doesn't match a revealed card then makes them both red and flips them
 function checkUnmatched(target) {
-    for(let card of initCards){
-        if(card.getAttribute('class').includes('revealed') && card.getAttribute('class') !== target.getAttribute('class')){
-            card.setAttribute('class', card.getAttribute('class').replace(/revealed/gi, 'false'));
-            target.setAttribute('class', target.getAttribute('class').replace(/revealed/gi, 'false'));
-            moves++;
-            setTimeout(() => {
-                hideCards(target);
-                for(let card of initCards){
-                    hideCards(card);
-                }
-            }, 500);
+  for (let card of initCards) {
+    if (
+      card.getAttribute('class').includes('revealed') &&
+      card.getAttribute('class') !== target.getAttribute('class')
+    ) {
+      navigator.vibrate(500);
+      card.setAttribute(
+        'class',
+        card.getAttribute('class').replace(/revealed/gi, 'false')
+      );
+      target.setAttribute(
+        'class',
+        target.getAttribute('class').replace(/revealed/gi, 'false')
+      );
+      moves++;
+      setTimeout(() => {
+        hideCards(target);
+        for (let card of initCards) {
+          hideCards(card);
         }
+      }, 500);
     }
+  }
 }
 /* End of checkUnmatched funtion */
 
 //To be called when the timer needs to be updated
-let timeStart =
-    setInterval(() => {
-        timer++;
-        timerSpan.innerHTML = timer;
-    },1000)
-;
+let timeStart = setInterval(() => {
+  timer++;
+  timerSpan.innerHTML = timer;
+}, 1000);
 /* End of upTimer funtion */
 
 //Open's the modal
 function openModal() {
-    clearInterval(timeStart);
-    lastTime = timer;
-    modalMoves.innerHTML = moves;
-    modalTime.innerHTML = lastTime;
-    modalStars.innerHTML = stars;
-    modal.style.display = 'block';
+  clearInterval(timeStart);
+  lastTime = timer;
+  modalMoves.innerHTML = moves;
+  modalTime.innerHTML = lastTime;
+  modalStars.innerHTML = stars;
+  modal.style.display = 'block';
 }
 /* End of openModal funtion */
 
 //Closes the modal
 function closeModal() {
-    modal.style.display = 'none';
+  modal.style.display = 'none';
 }
 /* End of closeModal funtion */
 
-//To be called when a move is made to display the current # of moves 
+//To be called when a move is made to display the current # of moves
 function showMoves() {
-    movesSpan.innerHTML = moves;
+  movesSpan.innerHTML = moves;
 }
 /* End of showMoves funtion */
 
 //Embeds the icons in the cards until revealed
-document.addEventListener('DOMContentLoaded',() => {addRandomIcons();
-    timer = 0;
-    showMoves();
+document.addEventListener('DOMContentLoaded', () => {
+  addRandomIcons();
+  timer = 0;
+  showMoves();
 });
 
 //Flips card and reveals icon
 board.addEventListener('click', revealAndCheck);
 
 //Reshuffles the cards and restarts the game
-restartButton.addEventListener('click',() => {
-    addRandomIcons(); 
-    showMoves();
-    timer = 0;
+restartButton.addEventListener('click', () => {
+  addRandomIcons();
+  showMoves();
+  timer = 0;
 });
 
 //Closes the modal when the 'x' is clicked
@@ -239,14 +275,12 @@ closeBtn.addEventListener('click', closeModal);
 
 //Restarts the game from the modal
 modalRestart.addEventListener('click', () => {
-    closeModal();
-    addRandomIcons(); 
-    showMoves();
-    timer = 0;
-    timeStart =
-    setInterval(() => {
-        timer++;
-        timerSpan.innerHTML = timer;
-    },1000)
-;
+  closeModal();
+  addRandomIcons();
+  showMoves();
+  timer = 0;
+  timeStart = setInterval(() => {
+    timer++;
+    timerSpan.innerHTML = timer;
+  }, 1000);
 });
